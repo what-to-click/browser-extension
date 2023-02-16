77a9467a54d0c133ebbd30715cea8ccd7a9a3507
Marcin Wróblewski <nicram606@gmail.com>
2023-02-16 21:38:44 +0100

    Fix long-description PDF exports

--- extension/firefox/content/page/export.js
+++ extension/firefox/content/page/export.js
@@ -1,6 +1,21 @@
 export async function savePdf() {
   document.querySelectorAll('[wtc-editor]').forEach((element) => element.classList.add('hidden'));
+  const textareas = [];
+  document.querySelectorAll('[wtc-textarea]').forEach((textarea) => {
+    textareas.push(textarea);
+    textarea.style = '';
+    const span = new DOMParser().parseFromString(
+      textarea.outerHTML.replace('<textarea', '<span').replace(new RegExp('</textarea>$', 'gm'), '</span>'),
+      'text/html'
+    ).querySelector('span');
+    textarea.replaceWith(span);
+  });
   await browser.runtime.sendMessage({ type: 'savePdf' });
+  document.querySelectorAll('[wtc-textarea]').forEach((span, index) => {
+    const textarea = textareas[index];
+    span.replaceWith(textarea);
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  });
   document.querySelectorAll('[wtc-editor]').forEach((element) => element.classList.remove('hidden'));
 }
 
