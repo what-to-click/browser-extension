f5c0a4d15604f152cd0af70b8d4d0852d9c2ea2f
Marcin Wróblewski <nicram606@gmail.com>
2023-03-02 22:55:59 +0100

    WIP: Add OCR

--- extension/firefox/content/page.js
+++ extension/firefox/content/page.js
@@ -9,4 +9,15 @@ window.addEventListener('load', async () => {
   document.getElementById('saveHtml').addEventListener('click', saveHtml);
   document.querySelector('[autofocus]').focus();
   document.querySelector('h1').addEventListener('keyup', updateMeta);
+
+  const formData = new FormData()
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  const img = document.querySelector('.screenshot');
+  canvas.width = img.naturalWidth;
+  canvas.height = img.naturalHeight;
+  context.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
+  formData.append('image', canvas.toDataURL('image/jpeg'))
+
+  fetch('http://localhost:3000', { method: 'POST', body: formData, }).then(console.info).catch(console.error)
 });
---
+++ ocr-server/app.js
@@ -0,0 +1,24 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  res.status(404)
+});
+
+module.exports = app;
