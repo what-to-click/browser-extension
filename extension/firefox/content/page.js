6bbdbf875bc01a0432272b86f0f66b06258df273
Marcin Wroblewski <contact@wrbl.xyz>
2023-03-04 20:03:00 +0100

    Add local tesseract

--- extension/firefox/content/page.js
+++ extension/firefox/content/page.js
@@ -1,6 +1,7 @@
 import { updateMeta } from './page/dom/seo.js';
 import { savePdf, saveMarkdown, saveHtml } from './page/export.js';
 import { main } from './page/dom/init.js';
+import * as ocr from './deps/tesseract@4.0.2.min.js';
 
 window.addEventListener('load', async () => {
   await main();
@@ -10,14 +11,17 @@ window.addEventListener('load', async () => {
   document.querySelector('[autofocus]').focus();
   document.querySelector('h1').addEventListener('keyup', updateMeta);
 
-  const formData = new FormData()
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d');
-  const img = document.querySelector('.screenshot');
-  canvas.width = img.naturalWidth;
-  canvas.height = img.naturalHeight;
-  context.drawImage(img, 0, 0, img.naturalWidth, img.naturalHeight);
-  formData.append('image', canvas.toDataURL('image/jpeg'))
+  const worker = await Tesseract.createWorker({
+    workerPath: './deps/worker@4.0.2.min.js',
+    workerBlobURL: false,
+    langPath: './deps/eng-fast.traineddata.gz',
+    corePath: './deps/tesseract-core@4.0.2.wasm.js'
+  });
 
-  fetch('http://localhost:3000', { method: 'POST', body: formData, }).then(console.info).catch(console.error)
+  const { data: { text } } = await worker.recognize(
+    document.querySelector('.screenshot'),
+    'eng',
+    { logger: m => console.log(m) }
+  );
+  console.debug(text);
 });
