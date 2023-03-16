1780422253c0f09ee3b12d2edd17840e045da585
Marcin Wróblewski <nicram606@gmail.com>
2023-03-16 22:30:32 +0100

    Fix parallel OCR

--- ocr-server/service/ocr/ocr.js
+++ ocr-server/service/ocr/ocr.js
@@ -1,26 +1,29 @@
 const Tesseract = require('tesseract.js');
 const path = require('path');
 
-let worker;
+let scheduler;
 
-async function initWorker() {
-  const w = await Tesseract.createWorker({
-    langPath: path.join(__dirname, 'data'),
-    tessedit_create_hocr: '0',
-    tessedit_create_tsv: '0',
-    tessedit_create_box: '0',
-    tessedit_create_unlv: '0',
-    tessedit_create_osd: '0',
-    errorHandler: e => console.error(e)
-  });
-  await w.loadLanguage('eng-fast');
-  await w.initialize('eng-fast');
-  worker = w;
-  return w;
+async function initWorker({ workerCount = 2 } = {}) {
+  let coldScheduler = Tesseract.createScheduler();
+  for (const _ of Array(workerCount).fill(0)) {
+    const worker = await Tesseract.createWorker({
+      langPath: path.join(__dirname, 'data'),
+      tessedit_create_hocr: '0',
+      tessedit_create_tsv: '0',
+      tessedit_create_box: '0',
+      tessedit_create_unlv: '0',
+      tessedit_create_osd: '0',
+      errorHandler: e => console.error(e)
+    });
+    await worker.loadLanguage('eng-fast');
+    await worker.initialize('eng-fast');
+    coldScheduler.addWorker(worker);
+  }
+  scheduler = coldScheduler;
 }
 
 async function recognizeWords(element) {
-  const result = await worker.recognize(element);
+  const result = await scheduler.addJob('recognize', element);
   return result.data.paragraphs.map(({ lines }) => {
     return lines.map((line) => line.words.map((word) => {
       return {
